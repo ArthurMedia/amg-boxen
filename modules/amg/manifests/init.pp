4f5f55e8334e23bac2fd2d60e3a412fd3dcbc97b
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-07-30 16:54:32 +0200

    Add amg module :
    * Common tools : iTerm2, Chrome, Firefox [...]
    * PHP-lol, Node & Ruby settings
    * Misc packages : htop, curl, ssh-copy-id [...]

---
+++ modules/amg/manifests/init.pp
@@ -0,0 +1,33 @@
+class amg {
+  # common tools
+  include iterm2::stable
+  include virtualbox
+  include chrome
+  include chrome::canary
+  include firefox
+  include sequel_pro
+  include vlc
+  include openoffice
+  include gimp
+  include keepassx
+  include imageoptim
+
+  # PHP-lol
+  include php::5_4_17
+  include php::fpm::5_4_17
+
+  class { 'php::global': version => '5.4.17' }
+
+  # Node
+  class { 'nodejs::global': version => 'v0.10.5' }
+
+  # Ruby
+  class { 'ruby::global': version => '2.0.0' }
+
+  # Misc
+  $useful = ['htop-osx', 'curl', 'ssh-copy-id', 'pwgen',]
+
+  package { $useful:
+    ensure => latest,
+  }
+}
